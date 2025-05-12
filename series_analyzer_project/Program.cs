0241bca9db7fd2b6e0fb83fde4ace1c014c8e627
benjyp2003 <benjypfeffer@gmail.com>
2025-05-12 17:54:49 +0300

    added func convertToInt()

--- series_analyzer_project/Program.cs
+++ series_analyzer_project/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,35 @@ namespace series_analyzer_project
     {
         static void Main(string[] args)
         {
-
+            convertToInt(args);
         }
+
+        // func that converts the series from string[] to int[]. (and validates that only numbers were givin)
+        static void convertToInt(string[] args)
+        {
+            int[] numbers = new int[args.Length];
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (int.TryParse(args[i], out int num))
+                {
+                    numbers[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine("enter numbers only! ");
+                    enterSeries();
+                }
+            }
+            menu(numbers);
+        }
+
+        
+
     }
 }
